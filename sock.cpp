529e64b394e1eb23ac47856c1ba8ea896093e63c
Vincent Pierce <vinhpierce@gmail.com>
2025-11-16 06:41:36 -0800

    initial commit

---
+++ sock.cpp
@@ -0,0 +1,20 @@
+/**************************************************************************************
+ * Author: Vincent Pierce 
+ * 
+ * Description: COMPE560 Network Programming Project -- Introduction to Sockets.
+ * This project accomplishes inter-process communication via 
+ * sockets. A struct containing an integer, character and float is passed between a 
+ * client and server before being printed out. In total three processes, two clients, 
+ * and one server will be utilized. 
+ * 
+ * 
+ * ***********************************************************************************/
+
+/* FILE INCLUSIONS ****************************************************************/
+#include sock.h
+#include <sys/socket.h>
+
+void main()
+{
+    
+}
---
+++ sock.h
@@ -0,0 +1,27 @@
+/**************************************************************************************
+ * 
+ * 
+ * 
+ * ***********************************************************************************/
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+/* LIBRARY INCLUSIONS ****************************************************************/
+#include <cmath>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+/* LOCAL DEFINITIONS *****************************************************************/
+struct 
+{
+    int  number;
+    char letter;
+    double_t decimal;
+
+}primitiveStruct;
+
+/* FUNCTION PROTOTYPES ***************************************************************/
+#endif 
